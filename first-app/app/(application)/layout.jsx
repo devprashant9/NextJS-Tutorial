79b2896b320ca8e99d647bfe892007bf7a005687
devprashant <kumarjha183@gmail.com>
2025-10-12 11:23:55 +0530

    route groups

---
+++ first-app/app/(application)/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MarketingLayout = ({children}) => {
+  return (
+    <div>
+      <h2>Marketing Layout</h2>
+      {children}
+    </div>
+  )
+}
+
+export default MarketingLayout
--- first-app/app/layout.tsx
+++ first-app/app/layout.tsx
@@ -7,17 +7,7 @@ export default function RootLayout({ children,
   return (
     <html lang="en">
       <body>
-        <header style={{
-          height: "4rem",
-          backgroundColor: "tomato",
-          color: "white"
-        }}>This is Root Header</header>
         {children}
-        <footer style={{
-          height: "4rem",
-          backgroundColor: "tomato",
-          color: "white"
-        }}>Footer</footer>
       </body>
     </html>
   );
