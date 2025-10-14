bff0218972c2b41953fb72624ec8c8379391ca97
devprashant <kumarjha183@gmail.com>
2025-10-14 11:57:00 +0530

    render server component as client component

---
+++ server-as-client/components/ServiceList.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ServiceList({children}) {
+
+  return (
+    <>
+      <h3>All Services List</h3>
+      <ul className="services-list">
+        {children}
+      </ul>
+    </>
+  );
+}
