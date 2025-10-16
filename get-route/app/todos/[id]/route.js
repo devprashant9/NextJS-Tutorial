8cb0fec4782d57b426112de0cc6bd3713a2ed667
devprashant <kumarjha183@gmail.com>
2025-10-16 21:42:22 +0530

    POST, PATCH, DELETE methods

--- get-route/app/todos/[id]/route.js
+++ get-route/app/todos/[id]/route.js
@@ -2,8 +2,21 @@ import data from "../../../data.json"
 
 export async function GET(request, { params }) {
     const { id } = await params;
-    console.log(id)
+    return Response.json(data[Number(id) - 1])
+}
 
+export async function PATCH(request, { params }) {
+    let { id } = await params;
+    const valueToUpdate = await request.json();
 
-    return Response.json(data[Number(id)])
+    const updatedData = data.map((item) => item.id === id ? {...item, text: valueToUpdate.text} : item)
+    return Response.json(updatedData);
+}
+
+export async function DELETE(request, { params }) {
+    const { id } = await params;
+    
+    const updatedData = data.filter((item) => id !== item.id);
+
+    return Response.json(updatedData);
 }
--- get-route/app/todos/route.js
+++ get-route/app/todos/route.js
@@ -1,9 +1,22 @@
-import data from "../../data.json"
+import data from "../../data.json";
+import fs from "fs/promises"
 
 export function GET(request) {
-  console.log("Running GET");
+  // console.log("Running GET");
   // return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } })
-  console.log(request);
+  // console.log(request);
   return Response.json(data);
 
+}
+
+export async function POST(request) {
+  const newData = await request.json();
+  // console.log(newData);
+  data.push(newData);
+
+  await fs.writeFile("data.json", JSON.stringify(data, null, 2));
+
+  return Response.json({ success: true, message: "ok", data }, {
+    status: 201
+  })
 }
