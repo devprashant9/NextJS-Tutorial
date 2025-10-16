65a3409cd4710a894bd7c052c372b6b47c623927
devprashant <kumarjha183@gmail.com>
2025-10-16 10:49:55 +0530

    understanding request object

--- get-route/app/todos/route.js
+++ get-route/app/todos/route.js
@@ -1,8 +1,9 @@
 import data from "../../data.json"
 
-export function GET() {
+export function GET(request) {
   console.log("Running GET");
   // return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } })
+  console.log(request);
   return Response.json(data);
 
 }
